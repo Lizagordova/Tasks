5c8e303e5d78c07c81b9c974ea77983539375932
Лизонька <lizunchik_737@mail.ru>
2022-02-15 23:32:29 +0300

    1-3 методичка

---
+++ Task-2-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_2_3
+{
+    class Program
+    {
+        static bool IsOdd(double n)
+        {
+            return n % 2 == 1;
+        }
+
+        static bool IsPositive(double n)
+        {
+            return n > 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            double number = -1;
+            double sum = 0;
+            while (number != 0)
+            {
+                Console.Write("Введите число:");
+                number = double.Parse(Console.ReadLine());
+                if (IsOdd(number) && IsPositive(number))
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine("Сумма нечётных положительных чисел: {0}", sum);
+        }
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя:");
+            string firstName = Console.ReadLine();
+            Console.Write("Введите фамилию:");
+            string lastName = Console.ReadLine();
+            Console.Write("Введите рост:");
+            int height = int.Parse(Console.ReadLine());
+            Console.Write("Введите вес:");
+            int weight = int.Parse(Console.ReadLine());
+            // склеивание
+            string info = "Имя:" + firstName + "; фамилия:" + lastName + "; рост:" + height + "; вес:" + weight;
+            Console.WriteLine("Cклеивание:");
+            Console.WriteLine(info);
+            // форматированный вывод
+            Console.WriteLine("Форматированный вывод:");
+            Console.WriteLine("Имя: {0}; фамилия: {1}; рост: {2}; вес: {3}", firstName, lastName, height, weight);
+            // вывод со знаком $
+            Console.WriteLine("Вывод со знаком $:");
+            Console.WriteLine($"Имя:{firstName}; фамилия:{lastName}; рост={height}; вес={weight}");
+        }
+    }
+}
