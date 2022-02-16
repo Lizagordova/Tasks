530de843145a55082f028df325643caa02d57ba0
Гордова Елизавета Сергеевна <GordovaES@corp.loc>
2022-02-16 18:09:48 +0300

    8 методичка 1 задание

---
+++ Task-8-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Гуркова Ирина");
+            Type type = typeof(DateTime);
+            var properties = type.GetProperties();
+            for (int i = 0; i < properties.Length; i++)
+            {
+                Console.WriteLine("Name={0}", properties[i].Name);
+            }
+            
+        }
+    }
+}
